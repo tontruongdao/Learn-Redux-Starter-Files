d7a41593a9f96666589caf1f6c935c3f4162729c
Truong Dao <truong.dao.do@gmail.com>
2021-03-12 11:59:50 -0500

    lesson2

---
+++ learn-redux/client/components/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router'
+
+const Main = React.createClass({
+    render() {
+        return (
+            <div>
+                <h1>Reduxtagram</h1>
+            </div>
+        )
+    }
+})
+
+export default Main
--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,1 +1,8 @@
-// let's go!
+import React from 'react';
+import { render } from 'react-dom';
+
+import CSS from './styles/style.styl'
+import Main from './components/Main'
+
+render(<Main/>, document.getElementById('root'))
+
